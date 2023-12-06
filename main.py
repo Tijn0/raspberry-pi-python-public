2ccf63396318133b9fc05ab8c343439b288f6b63
Tijn <tijnheijboer@gmail.com>
2023-12-06 15:33:36 +0100

    vff

--- main.py
+++ main.py
@@ -11,9 +11,9 @@ print("starting")
 def main():
     while True:
         GPIO.output(4, GPIO.HIGH)
-        time.sleep(0.0006)
+        time.sleep(0.0003)
         GPIO.output(4, GPIO.LOW)
-        time.sleep(0.0006)
+        time.sleep(0.0003)
 
 
 if __name__ == "__main__":
